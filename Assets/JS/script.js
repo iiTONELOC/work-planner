239f558ac3925c7b3e4d3f4313dceb58b88f9d23
iiTONELOC <anthonytropeano@me.com>
2021-01-22 13:26:33 -0500

    set interval to check for status change

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -10,7 +10,7 @@ console.log(dayWeek);
     $('#currentDay').text(dayWeek); 
 },30000);
 
-//time-block status
+//timeblock status
 var currentTime=moment().format('H');
 console.log("current time: " + currentTime)
 $(document).ready(function(){ 
@@ -34,6 +34,23 @@ $(document).ready(function(){
 
    
 //auditTime();
+setInterval(function(){ 
+    
+    $("textarea").each(function(index){
+        $(this).removeClass("past future present")
+        if($(this).attr("data-value")===currentTime){
+        console.log("Yay you found the current time!");
+        $(this).addClass("present");
+    }else if($(this).attr("data-value") >currentTime){
+            console.log("This is the FUTURE!");
+            $(this).addClass("future");
+        }else if($(this).attr("data-value") <currentTime){
+            $(this).addClass("past");
+            console.log("this is the past")
+        }
+    });
+
+},60000);
 
 
 

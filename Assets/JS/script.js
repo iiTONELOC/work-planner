bcba96df44ee7754a429cbdf462e1bcff2fd9bb4
iiTONELOC <anthonytropeano@me.com>
2021-01-22 02:13:12 -0500

    added gird and icons

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,3 +3,16 @@ var dayWeek = moment().format('LLLL');
 console.log(dayWeek);
 
 $('#currentDay').text(dayWeek);
+
+//turn blank p elements into input fields
+$('#container').on('click','#description', function(){
+    
+
+    var textBox=$("<textarea>")
+    .addClass("ts col-10 description past");
+
+    $(this).replaceWith(textBox);
+});
+   // turn them back upon save
+
+

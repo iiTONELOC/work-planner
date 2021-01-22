2496be6a4ed029841543ccaa495cf0f54619c93c
iiTONELOC <anthonytropeano@me.com>
2021-01-22 10:54:02 -0500

    got the text area to display properly removed the div

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,28 +1,40 @@
 //display current day and time
-
 var dayWeek = moment().format('LLLL');
 console.log(dayWeek);
-
 $('#currentDay').text(dayWeek);
 
 //update the time every 30 seconds
-
 var timeUpdate= setInterval(function(){
     let dayWeek = moment().format('LLLL');
 console.log(dayWeek);
     $('#currentDay').text(dayWeek); 
 },30000);
 
+//timeblock status
+
+var auditTime = function(){
+    console.log($(".row"))
+}
+auditTime();
+
+
+
+
+
+
+
+
+
 
 //turn blank p elements into input fields
-$('#container').on('click','#description', function(){
+//$('#container').on('click','#description', function(){
     
 
-    var textBox=$("<textarea>")
-    .addClass("ts col-10 description past");
+   // var textBox=$("<textarea>")
+    //.addClass("ts col-10 description past");
 
-    $(this).replaceWith(textBox);
-});
+    //$(this).replaceWith(textBox);
+//});
    // turn them back upon save
 
 
